6c8168fa78e47f93bb0c37d9822d4424614e46b5
AlexKroshkov <redsoul365@yandex.ru>
2021-06-19 22:28:13 +0500

    create: js validate and mask. parallax

--- js/main.js
+++ js/main.js
@@ -1,63 +1,92 @@
-$(document).ready (function () {
-  const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
     // Optional parameters
     loop: true,
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.hotel-slider__button--next',
-      prevEl: '.hotel-slider__button--prev',
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
     },
     effect: "coverflow",
-    
+
     keyboard: {
       enabled: true,
       onlyInViewport: true,
-    }
-  });
-  
-  const reviewsSlider = new Swiper('.reviews-slider', {
-    // Optional parameters
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.reviews-slider__button--next',
-      prevEl: '.reviews-slider__button--prev',
     },
   });
-  
-  var menuButton = document.querySelector('.menu-button');
-  menuButton.addEventListener('click', function () {
-    console.log('Клик по кнопке меню');
-    document.querySelector('.navbar-bottom')
-    .classList.toggle('navbar-bottom--visible');
+
+  $(".newslatter").parallax({
+    imageSrc: "images/newslatter-bg.jpg",
+    speed: "0.5",
   });
 
-  var modalButton =$('[data-toggle=modal]');
-  var closeModalButton =$('.modal__close');
-  modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal)
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
 
-  document.addEventListener('keydown', function(event) {
-    if (event.key == 'Escape') {
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key == "Escape") {
       closeModal(event);
     }
   });
 
   function openModal() {
-    var modalOverlay = $('.modal__overlay');
-    var modalDialog = $('.modal__dialog');
-    modalOverlay.addClass('modal__overlay--visible');
-    modalDialog.addClass('modal__dialog--visible');
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
   }
 
   function closeModal(event) {
-    event.preventDefault()
-    var modalOverlay = $('.modal__overlay');
-    var modalDialog = $('.modal__dialog');
-    modalOverlay.removeClass('modal__overlay--visible');
-    modalDialog.removeClass('modal__dialog--visible');
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
   }
-});
 
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Your name must contain at least 2 symbols",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "We need your phone number to contact you",
+          minlength: "Your phone must contain at least 11 numbers",
+        },
+      },
+    });
+  });
+
+  $(document).ready(function () {
+    $(".phone").mask("+7 (000) 000-00-00");
+  });
+});
