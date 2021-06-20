d8bcd078672b32d8fe995cf7af314cd2755f1a9e
AlexKroshkov <redsoul365@yandex.ru>
2021-06-20 17:10:31 +0500

    fix: modal and footer

--- js/main.js
+++ js/main.js
@@ -40,15 +40,20 @@ $(document).ready(function () {
       .classList.toggle("navbar-bottom--visible");
   });
 
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom__visible");
+  });
+
   var modalButton = $("[data-toggle=modal]");
   var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
-
-  document.addEventListener("keydown", function (event) {
-    if (event.key == "Escape") {
-      closeModal(event);
-    }
+  
+  document.addEventListener("keydown", function(event) {
+   if (event.key == "Escape") {
+     closeModal(event);
+   }
   });
 
   function openModal() {
@@ -63,7 +68,8 @@ $(document).ready(function () {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
+    modalDialog.removeClass("modal__dialog--visible");    
+    
   }
 
   $(".form").each(function () {
